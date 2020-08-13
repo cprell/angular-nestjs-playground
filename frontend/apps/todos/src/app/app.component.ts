38d09c80a951d521213d558203af6eda6b2a74b3
Christian Prell <chr.prell@googlemail.com>
2020-08-13 16:24:51 +0200

    further changes

--- frontend/apps/todos/src/app/app.component.ts
+++ frontend/apps/todos/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 interface Todo {
   title: string;
@@ -12,9 +13,17 @@ interface Todo {
 export class AppComponent {
   todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
 
-  addTodo() {
-    this.todos.push({
-      title: `New todo ${Math.floor(Math.random() * 1000)}`,
+  constructor(private httpClient: HttpClient) {
+    this.fetchData();
+  }
+
+  public fetchData() {
+    this.httpClient.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
+  public addTodo() {
+    this.httpClient.post('/api/addTodo', {}).subscribe(() => {
+      this.fetchData();
     });
   }
 }
